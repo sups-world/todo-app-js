6be2bd08d75018c31c6159113f502ceb09c18cc0
Suprajna Sharma <suprasubedi11@gmail.com>
2023-10-28 20:14:46 +0545

    cross sign enlarges on hover

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML =inputBoxEl.value;
         listContainerEl.appendChild(li);
+
+        // to show cross icon..multiplication icon
+        let span = document.createElement("span");
+        span.innerHTML ='\u00d7';
+        li.appendChild(span)
     }
     // to clear input field
     inputBoxEl.value ='';
